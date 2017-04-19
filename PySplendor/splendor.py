132a23a6810db217af65dac7721d6cbad81f6b0f
green-fr <green_fr@yahoo.com>
2017-04-19 22:45:52 +0200

    Add UI classes

--- PySplendor/splendor.py
+++ PySplendor/splendor.py
@@ -1,7 +1,11 @@
-from game import *
+from game_splendor1 import *
+from game_ui_splendor import *
 from playerUI import *
 
-game = Game()
+game = GameSplendor()
+game_ui = GameUISplendor()
+game.attach_ui(game_ui)
+
 game.load_card_surfaces()
 game.show_deck_and_desk()
 game.show_tokens()
